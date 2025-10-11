21dc6349c6e9e2569b0b2f1d7b73d64c9edc8f30
superstar-zimtk <takuemsax@gmail.com>
2025-10-11 19:42:20 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'ᴘʀᴇᴛᴛʏ ᴍᴅ',
+  author: '‎',
+  botName: "ᴘʀᴇᴛᴛʏ ᴍᴅ",
+  botOwner: 'superstar', // Your name
+  ownerNumber: '263715024370', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.5.9",
+  updateZipUrl: "https://github.com/superstar-zimtk/pretty-private-repohide/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
