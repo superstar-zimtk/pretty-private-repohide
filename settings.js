0c51d43b74e198f3c5bdb9e64ba4a218a04b87fa
superstar-zimtk <takuemsax@gmail.com>
2025-10-12 18:36:55 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.5.9",
+  version: "1.6.0",
   updateZipUrl: "https://github.com/superstar-zimtk/pretty-private-repohide/archive/refs/heads/main.zip",
 };
 
